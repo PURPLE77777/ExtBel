559b682b55d2aaff83165f4c93a3f3a282e3e04a
PURPLE77777 <stefanovich.pasha@yandex.by>
2021-10-11 04:15:50 +0300

    half work

---
+++ src/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 2,
+        arrows: true,
+        autoplay: true,
+        slidesToScroll: 2,
+        dots: true,
+        variableWidth: true
+    });
+});

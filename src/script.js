e35e01ab538ddde68eca8e2453bef76e9ae14881
PURPLE77777 <stefanovich.pasha@yandex.by>
2021-10-14 13:18:45 +0300

    added animations

--- src/script.js
+++ src/script.js
@@ -29,64 +29,52 @@ let btnCloseLogIn = document.querySelector("label.btn-close");
 function showHeaderList(){
     let headerList = document.querySelector("div.header-list");
     let modaleBg = document.querySelector("div.modale-bg");
-    let modaleWindow = document.querySelector("div.login-window");
-
-    modaleWindow.style.display = "none";
 
     modaleBg.style.display = "block";
-    modaleBg.style.opacity = "1";
-    modaleBg.style.zIndex = "50";
+    modaleBg.style.animation = "showModaleBg .3s 1 linear forwards";
 
     headerList.style.display = "block";
-    headerList.style.left = "0px";
+    headerList.style.animation = "showHeaderList .3s 1 linear forwards";
 }
 
 function showHeaderSearch(){
     let headerSearch = document.querySelector("div.header-search");
 
     headerSearch.style.display = "block";
-    headerSearch.style.left = "0%";
+    headerSearch.style.animation = "showHeaderSearch .3s 1 linear forwards";
 }
 
 function closeHeaderList(){
     let headerList = document.querySelector("div.header-list");
     let modaleBg = document.querySelector("div.modale-bg");
 
-    modaleBg.style.opacity = "0";
-    headerList.style.left = "-590px";
-    
-    modaleBg.style.zIndex = "-1";
-    modaleBg.style.display = "none";
-    headerList.style.display = "none";
+    modaleBg.style.animation = "closeModaleBgMove .3s 1 linear forwards";
+
+    headerList.style.animation = "closeHeaderList .3s 1 linear forwards";
 }
 
 function closeHeaderSearch(){
     let headerSearch = document.querySelector("div.header-search");
 
-    headerSearch.style.left = "-100%";
-    headerSearch.style.display = "none";
+    headerSearch.style.animation = "closeHeaderSearch .3s 1 linear forwards";
 }
 
 function LogIn(){
-        let loginWindow = document.querySelector("div.login-window");
-        let modaleBg = document.querySelector("div.modale-bg");
-    
-        modaleBg.style.display = "block";
-        modaleBg.style.opacity = "1";
-        modaleBg.style.zIndex = "50";
-    
-        loginWindow.style.display = "block";
+    let loginWindow = document.querySelector("div.login-window");
+    let modaleBg = document.querySelector("div.modale-bg");
+
+    modaleBg.style.animation = "showModaleBg .3s 1 linear forwards";
+
+    loginWindow.style.animation = "showLoginWindow .3s 1 linear forwards";
 }
 
 function closeLogIn(){
     let loginWindow = document.querySelector("div.login-window");
     let modaleBg = document.querySelector("div.modale-bg");
 
-    modaleBg.style.opacity = "0";
-    modaleBg.style.zIndex = "-1";
-    modaleBg.style.display = "none";
+    modaleBg.style.animation = "closeModaleBgMove .3s 1 linear forwards";
 
-    loginWindow.style.display = "none";
+    loginWindow.style.animation = "closeLoginWindow .3s 1 linear forwards";
 }
 
 btnBurgerMenu.addEventListener("click", showHeaderList);
